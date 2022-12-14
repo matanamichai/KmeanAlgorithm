00f6ead05d2d2b99dd57c056d181b31e6e6b176e
ShahafLevin <shahaf.levinson@gmail.com>
2022-12-14 10:34:15 +0200

    add input validations and clean the code a bit

--- kmeans.c
+++ kmeans.c
@@ -1,20 +1,32 @@
-#define  _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
+#define  _POSIX_C_SOURCE 200809L
+#define MAX_ITER 1000
+
+typedef struct cord
 {
     double value;
     struct cord *next;
-} cord;
+} cord ;
 
-typedef struct 
+typedef struct vector
 {
+    cord *cords;
     struct vector *next;
-    struct cord *cords;
-} vector;
+} vector ;
+
+vector *fillDataPoint();
+int validateIter(char *iter);
+int isNaturalNumber(char *c);
+int isdigit(int);
+int validateK(char *k, vector *pointsVector);
+int countPointsInVector(vector *pointsVector);
+void printVector(vector*);
+void printCord(cord*);
+
 
 vector *fillDataPoint(){
     vector *head_vec, *curr_vec, *next_vec;
@@ -51,20 +63,111 @@ vector *fillDataPoint(){
         curr_cord->next = NULL;
     }
     return head_vec;
-
 }
 
+void printVector(vector *v) {
+    while (v->next != NULL) {
+        printCord(v->cords);
+        v = v->next;
+    }
+    printf("\n");
+}
 
+void printCord(cord *c) {
+    do {
+        printf("%.4f,", c->value);
+        c = c->next;
+    }
+    while (c->next != NULL);
+    printf("%.4f\n", c->value);
+}
+
+int validateIter(char *iter) {
+    int intIter;
+    
+    if (isNaturalNumber(iter) != 0) {
+        printf("Invalid maximum iteration!\n");
+        return 1;    
+    }
+
+    intIter = atoi(iter);
+    if (intIter >= MAX_ITER || intIter <= 1) {        
+        printf("Invalid maximum iteration!\n");
+        return 2;
+    }
+
+    return 0;
+}
+
+int validateK(char *k, vector *pointsVector) {
+    int points;
+    int intK;
+    
+    if (isNaturalNumber(k) != 0) {
+        printf("Invalid number of clusters!\n");
+        return 1;
+    }
+
+    points = countPointsInVector(pointsVector);
+    intK = atoi(k);
+    if (intK >= points || intK <= 1) {
+        printf("Invalid number of clusters!\n");
+        return 2;
+    }
+
+    return 0;
+}
+
+int countPointsInVector(vector *v) {
+    int counter = 0;
+    while (v->next != NULL) {
+        counter++;
+        v = v->next;
+    }
+
+    return counter;
+}
+
+// returns 0 if it is a natural number, and 1 otherwise
+int isNaturalNumber(char *c) {
+    while (*c != '\0') {
+        if (isdigit(*c) == 0) {
+            return 1;    
+        }
+
+        c++;
+    }
+
+    return 0;
+}
 
 int main(int argc, char *argv[]){
-    struct vector *headVector;
+    vector *pointsVector;
+    vector *KCenterVector;
     int maxOfIter;
+    int valid;
+    int k;
+
     if (argc > 2){
+        valid = validateIter(argv[2]);
+        if (valid != 0) {
+            return 1;
+        }
+
         maxOfIter = atoi(argv[2]);
     }
     else{
         maxOfIter = 200;
     }
     
-    headVector = fillDataPoint();
+    pointsVector = fillDataPoint();
+    valid = validateK(argv[1], pointsVector);
+    if (valid != 0) {
+        return 1;
+    }
+
+    k = atoi(argv[1]);
+
+    printVector(pointsVector);   
+    return 0;
 }
